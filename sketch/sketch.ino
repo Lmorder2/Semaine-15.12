0b892821c92b0bd1ca51cceb9fec224cecb10966
Lmorder2 <lmorder2@users.noreply.github.com>
2025-12-15 13:33:07 +0100

    test panneau LED

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,21 @@
+#include <Arduino_LED_Matrix.h>
+
+ArduinoLEDMatrix matrix;
+
+uint8_t motif[8][13] = {
+  {1,0,1,1,1,1,1,0,0,1,0,0,1},
+  {1,0,1,1,0,0,1,0,0,1,0,0,1},
+  {1,1,1,1,1,0,1,0,0,1,0,0,1},
+  {1,0,1,1,0,0,1,1,1,1,1,1,1},
+  {1,0,1,1,1,1,1,1,0,1,0,0,1},
+  {1,0,1,1,0,1,1,0,1,1,0,0,1},
+  {0,0,0,0,0,1,1,1,0,1,0,0,1},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
 void setup() {
-  // put your setup code here, to run once:
-
+  matrix.begin();
+  matrix.renderBitmap(motif, 8, 13);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
